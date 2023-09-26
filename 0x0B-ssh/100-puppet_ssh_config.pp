59ac46ce52f6e4a1c71eb9f0bfdc8a6f05883612
tobitprince <prince.tobit@strathmore.edu>
2023-09-26 09:36:14 +0300

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Set the config of a ssh_file
 file_line { 'Change the main private key':
   path => '/etc/ssh/ssh_config',
-  line => '    IdentityFile ~/.ssh/id_rsa',
+  line => '    IdentityFile ~/.ssh/school',
 }
 
 file_line { 'No Authenticate with passowrd':
